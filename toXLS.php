804bf3f68784a568dab48ce92cb7dd438ce10ace
breadwithmeth <58703662+breadwithmeth@users.noreply.github.com>
2022-09-08 14:24:52 +0600

    Add files via upload

--- toXLS.php
+++ toXLS.php
@@ -11,6 +11,19 @@ for($i=1; $i<100; $i++){
 
 $writer = new XLSXWriter();
 $writer->writeSheet($table);
-$writer->writeToFile('output.xls');
+$temp_file = tempnam(sys_get_temp_dir(), 'test.xls');
+$writer->writeToFile($temp_file);
+if (file_exists($temp_file)) {
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename="test.xls"');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($temp_file));
+    readfile($temp_file);
+    exit;
+}
+
 ?>
 
